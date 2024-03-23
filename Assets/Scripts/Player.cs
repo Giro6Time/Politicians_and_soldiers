5bede9eee05e29e6c7d07a966b87dad74983f455
Violet <1767645776@qq.com>
2024-03-23 21:09:39 +0800

    合并版本4.1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Player
+public class Player: Singleton<Player>
 {
-    //TODO：创建单例模式的基类
-    public static Player Instance
-    { 
-        get
-        {
-            if(instance == null)
-                instance = new Player();
-            return instance;
-        }
-        private set
-        {
-            instance = value;
-        }
-    }
-    private static Player instance;
-
+    
     /// <summary>
     /// 决策点
     /// </summary>
@@ -46,28 +31,11 @@ public class Player
     /// 兵力补给
     /// </summary>
     public float troopIncrease;
-
-}
-=======
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[System.Serializable]
-public class Player: Singleton<Player>
-{
-    public int decisionValue;
+    
     public int decisionValueMax;
-    public float sanity;
-    public float armament;
-    public float fund;
-    public float popularSupport;
-    public float troopIncrease;
 
     internal void Init()
     {
         //TODO: 锟斤拷始锟斤拷值为锟斤拷值锟斤拷锟斤拷锟竭讹拷取PlayerSO锟斤拷始锟斤拷值
     }
 }
->>>>>>> develop
