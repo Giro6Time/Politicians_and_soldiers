88f0b446098026c7898e25f279fe5709ef4ca88e
Giro6Time <3139106326@qq.com>
2024-05-01 14:51:42 +0800

    删除一些没用的using

--- Assets/Scripts/WorkScript_Pigeon/ArmyManager.cs
+++ Assets/Scripts/WorkScript_Pigeon/ArmyManager.cs
@@ -1,11 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Net;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Purchasing;
 
 public class ArmyManager : MonoBehaviour
 {
